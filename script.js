08ad73ffe63ce3be99da42a2d4fda825bbdfd643
Sohail makandar <sohailmakandar55@gmail.com>
2024-11-15 15:46:03 +0530

    Add files via upload
    PORT FOLIIO  PROJECT

---
+++ script.js
@@ -0,0 +1,6 @@
+// Add JavaScript for interactivity if needed
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+     event.preventDefault();
+     alert("Thank you for contacting me!");
+   });
+   
